55a0e86c9efe1e8b1e627db2e7ad6df341fe7896
amiroof54 <amiroo.f54@gmail.com>
2024-02-05 14:15:05 +0330

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
-
-if [[ ! -z $(which npm)]]; then
-    echo "npm found"
-else
-    if [[ ! -z $(which yum) ]]; then
-        sudo yum install npm -y
-    elif [[ ! -z $(which apt) ]]; then
-        sudo apt install npm -y
-    elif [[ ! -z $(which pacman)]] then
-        sudo pacman -S npm --noconfirm
-    else
-        echo "please install npm"
-        exit
-    fi
-fi
-
+cd gui
+npm run build
+cd ..
 node ./backend/main.js
