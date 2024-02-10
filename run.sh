1638b46754220dc5eb9c772ce5b284eca4c3b08b
amiroof54 <amiroo.f54@gmail.com>
2024-02-10 16:59:10 +0330

    Changed run.sh and vue package.json. to optimize prodoction and cut unneccary builds.

--- run.sh
+++ run.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-mkdir frontend
-cd gui
-npm run build
-cd ..
+frontend_install_dir="./gui/node_modules"
+backend_install_dir="./backend/node_modules"
+if [ ! -d "$frontend_install_dir" ]; then
+    cd gui
+    npm install
+    cd ..
+fi
+if [ ! -d "$backend_install_dir" ]; then
+    cd backend
+    npm install
+    cd ..
+fi
+frontend_dir="./frontend"
+choice="Y" 
+if [ -d "$frontend_dir" ]; then
+    read -p "Do you want to build the frontend? (Y/n): " choice
+fi
+
+if [ ! "$choice" = "n" ]; then
+    mkdir frontend
+    cd gui
+    npm run build
+    cd ..
+fi
+
 node ./backend/main.js
